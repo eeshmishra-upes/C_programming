cfaa68df12c9cd3dd1929800c00ad04d8f224cd6
eeshmishra2306 <eesh.mishra2306@gmail.com>
2025-12-19 14:45:20 +0530

    Add C source files and ignore executables

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 
 int main
 () {
-    printf("Hello, World!\n");
+// This is my first C program
+
+/*
+ye 
+bhi 
+ek 
+cmnt
+hai
+
+*/
+
+
+    printf("I like Seedhe Maut\n");
+    printf("They're really good!\n");
+
     return 0;
 }
