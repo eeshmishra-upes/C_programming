ac4423931b7e202b320d741725fbff3fbf55fd03
Eesh Mishra <EESH.24334@stu.upes.ac.in>
2025-12-19 21:24:39 +0530

    x``

---
+++ L2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{ // we are gonna learn bout variables in c 
+    int age = 19;
+    int year = 2025;
+    int quantity = 4;
+    printf("thTe year is %d\n",year);
+    printf("My age is %d\n", age);
+     printf("We are a family of %d", quantity);
+
+    return 0;
+}
