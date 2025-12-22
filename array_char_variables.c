bb6528d83ef2f1745b0de91a919335d0b0bdca4b
eeshmishra-upes <EESH.24334@stu.upes.ac.in>
2025-12-22 16:41:57 +0530

    Added C variable examples

---
+++ array_char_variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{ 
+    char vodka [] = "Absolute Dhoka";
+
+    printf("I like to have %s\n",vodka);
+
+    return 0;
+}
---
+++ bool.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+
+    printf("Is programming fun? %s\n", isProgrammingFun ? "true" : "false");
+    printf("Is fish tasty? %s\n", isFishTasty ? "true" : "false");
+
+    return 0;
+}
