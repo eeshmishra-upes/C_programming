320abbbfc8d493b6fadcc5afa5a2d66ad0c50747
eeshmishra-upes <EESH.24334@stu.upes.ac.in>
2025-12-22 17:36:49 +0530

    New

---
+++ hw2.c
@@ -0,0 +1,40 @@
+// I have to use all the variables learnt to create a program 
+#include <stdio.h>
+#include <stdbool.h>
+
+int main () {
+
+    char name [] = " Eesh";
+    int age = 19;
+    float height = 6.1;
+    double sgpa = 8.35279;
+    char grade = 'A';
+    bool IsOnline=1;
+
+    printf("My Name is %s\n",name);
+     printf("My age is %d\n",age);
+      printf("My height is %f\n",height);
+       printf("My sgpa is %lf\n",sgpa);
+        printf("My grade is %c\n",grade);
+
+       if(IsOnline)
+       { 
+        printf("I am Online\n");
+       }
+        else
+        {
+            printf("I am Offline\n");
+       }
+
+
+
+
+
+
+
+
+
+
+}
+
+
